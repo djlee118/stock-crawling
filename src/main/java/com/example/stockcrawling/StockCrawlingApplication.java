4998528440ae92edbf9173dc3132bb8f68fbb4be
djlee118 <djlee118@nate.com>
2022-03-15 18:46:58 +0900

    stock-crawling 시작!

---
+++ src/main/java/com/example/stockcrawling/StockCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockcrawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockCrawlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockCrawlingApplication.class, args);
+    }
+
+}
